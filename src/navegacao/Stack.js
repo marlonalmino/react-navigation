37e826f42afd888d05d2f74e9b0a48e137eac1a7
Marlon Almino <alminomarlon@gmail.com>
2022-12-22 13:37:10 -0300

    Navegação Stack #03

--- src/navegacao/Stack.js
+++ src/navegacao/Stack.js
@@ -3,15 +3,30 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import TelaA from '../views/TelaA'
 import TelaB from '../views/TelaB'
 import TelaC from '../views/TelaC'
+import PassoStack from '../components/PassoStack'
 
 const Stack = createNativeStackNavigator()
 
 export default props => (
   <Stack.Navigator initialRouteName='TelaA'
     screenOptions={{ headerShown: true }} >
-    <Stack.Screen name='TelaA' component={TelaA}
-      options={{ title: 'Informações Iniciais' }} />
-    <Stack.Screen name='TelaB' component={TelaB} />
+
+    <Stack.Screen name='TelaA'
+      options={{ title: 'Informações Iniciais' }}>
+      {props => (
+        <PassoStack {...props} avancar='TelaB'>
+          <TelaA />
+        </PassoStack>
+      )}
+    </Stack.Screen>
+
+    <Stack.Screen name='TelaB'>
+      {props => (
+        <PassoStack {...props} avancar='TelaC' >
+          <TelaB />
+        </PassoStack>
+      )}
+    </Stack.Screen>
     <Stack.Screen name='TelaC' component={TelaC} />
   </Stack.Navigator>
 )
--- src/views/TelaA.js
+++ src/views/TelaA.js
@@ -1,11 +1,8 @@
 import React from 'react'
 import TextoCentral from '../components/TextoCentral'
 
-export default props => {
-  props.navigation.navigate('TelaB')
-  return (
-    <TextoCentral corFundo='#E53935'>
-      Tela A
-    </TextoCentral>
-  )
-}
+export default props => (
+  <TextoCentral corFundo='#E53935'>
+    Tela A
+  </TextoCentral>
+)
