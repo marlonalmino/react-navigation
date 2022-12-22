43f9e183574e8befaed1a0f0804989c15d027226
Marlon Almino <alminomarlon@gmail.com>
2022-12-22 12:59:03 -0300

    Navegação Stack #02

--- src/views/TelaA.js
+++ src/views/TelaA.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import TextoCentral from '../components/TextoCentral'
 
-export default props => (
-  <TextoCentral corFundo='#E53935'>
-    Tela A
-  </TextoCentral>
-)
+export default props => {
+  props.navigation.navigate('TelaB')
+  return (
+    <TextoCentral corFundo='#E53935'>
+      Tela A
+    </TextoCentral>
+  )
+}
