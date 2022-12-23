c1cf5f1efa7c7686ccf53c4a28d3755633f600a1
Marlon Almino <alminomarlon@gmail.com>
2022-12-23 11:10:10 -0300

    Navegação Tabs

---
+++ src/navegacao/Tab.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import TelaA from '../views/TelaA'
+import TelaB from '../views/TelaB'
+import TelaC from '../views/TelaC'
+import PassoStack from '../components/PassoStack'
+
+const Tab = createBottomTabNavigator()
+
+export default props => (
+  <Tab.Navigator screenOptions={{
+    tabBarActiveTintColor: 'red',
+    tabBarInactiveTintColor: 'blue',
+    tabBarLabelStyle: { fontSize: 30 }
+  }} initialRouteName='TelaB'>
+    <Tab.Screen name='TelaA' component={TelaA} />
+    <Tab.Screen name='TelaB' component={TelaB} />
+    <Tab.Screen name='TelaC' component={TelaC} />
+  </Tab.Navigator>
+)
